5e6d7774816e01556393ea40dbffc9bf00d64496
potatozero <potatozero@126.com>
2020-11-17 19:43:36 +0800

    write exit()

---
+++ sys_program/part_3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t result;
+    result = fork();
+
+    if(result == -1)
+        printf("fork error!\r\n");
+    if(result == 0)
+    {
+        printf("parent");
+        _exit(0);
+    }
+    else
+    {
+        printf("son");
+        exit(0);
+    }
+}
+
